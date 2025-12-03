fcd51249e9d0efe1bace813d26dfffcc9d452947
Lucas Sorensen <lucas.alex.sorensen@gmail.com>
2025-12-03 21:31:00 +0100

    day 2 fast but ugly

--- python/d02fast.py
+++ python/d02fast.py
@@ -8,26 +8,42 @@ with open(f"../{"test-" if "-t" in sys.argv else ""}data/d02.txt", "r") as file:
     ]
 
 
-sequences = []
+def generate_repeated_patterns(seq_len: int, force_twice: bool = False):
+    pattern_len_range = [seq_len // 2] if force_twice else range(1, (seq_len // 2) + 1)
+    for pattern_len in pattern_len_range:
+        if seq_len % pattern_len != 0:
+            continue
+        # geometric series for repetition masks
+        factor = (10**seq_len - 1) // (10**pattern_len - 1)
+        start_n = 10 ** (pattern_len - 1)
+        stop_n = 10**pattern_len
+        for n in range(start_n, stop_n):
+            yield n * factor
 
 
-def sequences_of_length(n: int) -> list[int]:
-    mid = n // 2
-    start = 10 ** (mid - 1)
-    end = 10**mid
-    t = end + 1
-    return [t * i for i in range(start, end)]
+twice_sequences = [
+    s
+    for i in range(2, 11)
+    if i % 2 == 0
+    for s in generate_repeated_patterns(i, force_twice=True)
+]
+twice_sequences = sorted(set(twice_sequences))
 
 
-sequences = [s for i in range(2, 11) for s in sequences_of_length(i)]
-sequences = sorted(set(sequences))
+all_sequences = [s for i in range(2, 11) for s in generate_repeated_patterns(i)]
+all_sequences = sorted(set(all_sequences))
+
 
 result_p1 = 0
-invalid_p2 = []
+result_p2 = 0
 for r in ranges:
-    smallest_idx = bisect.bisect_left(sequences, r.start)
-    biggest_idx = bisect.bisect_right(sequences, r.stop) - 1
-    result_p1 += sum(sequences[smallest_idx : biggest_idx + 1])
+    smallest_idx = bisect.bisect_left(twice_sequences, r.start)
+    biggest_idx = bisect.bisect_right(twice_sequences, r.stop) - 1
+    result_p1 += sum(twice_sequences[smallest_idx : biggest_idx + 1])
+
+    smallest_idx = bisect.bisect_left(all_sequences, r.start)
+    biggest_idx = bisect.bisect_right(all_sequences, r.stop) - 1
+    result_p2 += sum(all_sequences[smallest_idx : biggest_idx + 1])
 
 print("p1", result_p1)
-print("p2", sum(invalid_p2))
+print("p2", result_p2)
