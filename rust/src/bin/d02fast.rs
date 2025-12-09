de68be8bfa77e536d8f59bb2aac711e84df23b0f
Lucas Sorensen <lucas.alex.sorensen@gmail.com>
2025-12-09 22:51:55 +0100

    Update d02fast.rs

--- rust/src/bin/d02fast.rs
+++ rust/src/bin/d02fast.rs
@@ -23,29 +23,27 @@ fn parse_input(input: &str) -> Result<ParsedInput> {
 }
 
 fn part_one(input: &ParsedInput) -> usize {
-    let sequences: Vec<_> = (2..=10)
+    let sequences = (2..=10)
         .skip(2)
-        .flat_map(|seq_len| generate_repeated_patterns(seq_len, true))
-        .collect();
+        .flat_map(|seq_len| generate_repeated_patterns(seq_len, true));
     solve(sequences, input)
 }
 
 fn part_two(input: &ParsedInput) -> usize {
-    let sequences: Vec<_> = (2..=10)
-        .flat_map(|seq_len| generate_repeated_patterns(seq_len, false))
-        .collect();
+    let sequences = (2..=10).flat_map(|seq_len| generate_repeated_patterns(seq_len, false));
     solve(sequences, input)
 }
 
-fn solve(mut sequences: Vec<usize>, input: &ParsedInput) -> usize {
-    sequences.sort_unstable();
-    sequences.dedup();
+fn solve(sequences: impl Iterator<Item = usize>, input: &ParsedInput) -> usize {
+    let mut seq_v = sequences.collect::<Vec<_>>();
+    seq_v.sort_unstable();
+    seq_v.dedup();
     input
         .iter()
         .map(|(start, end)| {
-            let smallest_idx = sequences.partition_point(|x| x < start);
-            let biggest_idx = sequences.partition_point(|x| x <= end);
-            sequences[smallest_idx..biggest_idx].iter().sum::<usize>()
+            let smallest_idx = seq_v.partition_point(|x| x < start);
+            let biggest_idx = seq_v.partition_point(|x| x <= end);
+            seq_v[smallest_idx..biggest_idx].iter().sum::<usize>()
         })
         .sum()
 }
@@ -54,9 +52,10 @@ fn generate_repeated_patterns(seq_len: usize, force_twice: bool) -> impl Iterato
     let half = seq_len / 2;
     let start = if force_twice { half } else { 1 };
 
-    (start..=half)
-        .filter(move |&pattern_len| seq_len.is_multiple_of(pattern_len))
-        .flat_map(move |pattern_len| geometric_series(seq_len, pattern_len))
+    let pattern_lens =
+        (start..=half).filter(move |&pattern_len| seq_len.is_multiple_of(pattern_len));
+
+    pattern_lens.flat_map(move |pattern_len| geometric_series(seq_len, pattern_len))
 }
 
 fn geometric_series(seq_len: usize, pattern_len: usize) -> impl Iterator<Item = usize> {
