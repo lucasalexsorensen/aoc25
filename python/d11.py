531a866be7221f9184141449073320bbc8abf375
Lucas Sorensen <lucas.alex.sorensen@gmail.com>
2025-12-11 10:54:07 +0100

    Update d11.py

--- python/d11.py
+++ python/d11.py
@@ -7,7 +7,7 @@ from utils import file_path
 
 with open(file_path(day=11), "r") as file:
     connections = {
-        from_node[:-1]: to_nodes
+        from_node.rstrip(":"): to_nodes
         for from_node, *to_nodes in [
             line.strip().split(" ") for line in file.readlines()
         ]
@@ -26,8 +26,8 @@ print("p1", dfs("you", "out"))
 print(
     "p2",
     sum(
-        prod(dfs(s, t) for s, t in pairwise(path))
-        for path in [
+        prod(dfs(s, t) for s, t in pairwise(steps))
+        for steps in [
             ("svr", "fft", "dac", "out"),
             ("svr", "dac", "fft", "out"),
         ]
